4b01e5cb2880bd9d410ff51aec1121a18939bc02
Graphicblues <a.s.kinch@gmail.com>
2014-10-26 14:52:05 -0700

    Cannon and Target scripts.

---
+++ Spencer/CollideTester.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollideTester : MonoBehaviour {
+
+	public bool red;
+	public string nextLevel;
+	void OnCollisionEnter2D (Collision2D info) {
+
+		if (red) {
+			if (info.gameObject.GetComponent<SpriteRenderer> ().color == new Color (1, 0, 0)) {
+				Destroy (gameObject);
+					Destroy (info.gameObject);
+				} 
+			else {
+
+					stTools.EndScene (nextLevel);
+
+				}
+			} 
+		else{
+
+			if (info.gameObject.GetComponent<SpriteRenderer> ().color == new Color (0, 0, 1)) {
+				Destroy (gameObject);
+				Destroy (info.gameObject);
+			}
+			else{
+
+				stTools.EndScene(nextLevel);
+
+			}
+		}
+
+
+	}
+}
+
---
+++ Spencer/MouseSpawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseSpawn : MonoBehaviour {
+	public AutoSpawner autoSpawnerRight;
+	public AutoSpawner autoSpawnerLeft;
+
+	void Update(){
+		
+		if (Input.GetMouseButtonDown (1)) {
+			
+			autoSpawnerRight.Spawn ();
+			
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			
+			autoSpawnerLeft.Spawn ();
+			
+		}
+	}
+}
